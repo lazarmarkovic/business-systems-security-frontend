82133b25c42d5a9c196e2a99f3a3c85b1463f9a9
Lazar Marković <lazarodvaljeva@gmail.com>
2019-06-17 00:54:21 +0200

    Finish front

--- src/app/components/certificate-tree/certificate-tree.component.ts
+++ src/app/components/certificate-tree/certificate-tree.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CertRequestImpl} from '../../models/Impl/cert-request-impl';
+import {CertificateService} from '../../services/certificate.service';
+import {TreeItem} from '../../models/treeItem';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-certificate-tree',
@@ -19,17 +22,62 @@ export class CertificateTreeComponent implements OnInit {
   private organizationalUnitOU: string;
   private commonNameCN: string;
 
-  constructor() {
+  private selectedNodeId: number;
+  private selectedNodeName: string;
+
+  constructor(private certService: CertificateService,
+              private toastrService: ToastrService) {
     this.nodes = null;
   }
 
   ngOnInit() {
     this.nodes = [];
     this.options = {};
+
+    this.fetchForest();
+  }
+
+  onTreeNodeSelect(event: any) {
+    this.selectedNodeId = event.node.data.id;
+    this.selectedNodeName = event.node.data.name;
+    this.data.issuer = this.selectedNodeName;
+  }
+
+  fetchForest() {
+    this.certService
+      .getForest()
+      .subscribe(
+        (forest: TreeItem[]) => {
+          this.nodes = forest;
+          this.toastrService.success('Forest is loaded.');
+        },
+        (err) => {
+          this.toastrService.error(err.error.apierror.message);
+        }
+      );
   }
 
   create() {
+    this.showSpinner = true;
+    this.data.subject =
+        'C=' + this.countryNameC + ', ' +
+        'O=' + this.organizationO + ', ' +
+        'OU=' + this.organizationalUnitOU + ', ' +
+        'CN=' + this.commonNameCN;
 
+    this.certService
+      .create(this.data)
+      .subscribe(
+        () => {
+          this.fetchForest();
+          this.toastrService.success('New certificate is created.');
+          this.showSpinner = false;
+        },
+        (err) => {
+          this.toastrService.error(err.error.apierror.message);
+          this.showSpinner = false;
+        }
+      );
   }
 
 
--- src/app/services/certificate.service.ts
+++ src/app/services/certificate.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment.prod';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Certificate} from '../models/certificate';
+import {CertRequest} from '../models/cert-request';
+import {TreeItem} from '../models/treeItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CertificateService {
 
-  constructor() { }
+  private readonly path = environment.baseUrl + '/api/certificates';
+
+  constructor(private http: HttpClient) {
+  }
+
+  get(id: any): Observable<Certificate> {
+    return this.http.get<Certificate>(this.path + `/${id}`);
+  }
+
+  getForest(): Observable<TreeItem[]> {
+    return this.http.get<TreeItem[]>(this.path + '/forest');
+  }
+
+  create(certificateCreateRequest: CertRequest): Observable<Certificate> {
+    return this.http.post<Certificate>(this.path, certificateCreateRequest);
+  }
 }
